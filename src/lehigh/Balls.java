c0fd3ffb8c7b81b277dce3d388ea14331b1ebd85
wuzho <zhw324@lehigh.edu>
2020-12-16 07:24:29 +0800

    Zhongjie Wu

---
+++ src/lehigh/Balls.java
@@ -0,0 +1,59 @@
+package lehigh;
+
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+
+public class Balls extends PApplet {
+    class Ball{
+        int x, y, radius;
+        float speedx, speedy;
+        int [] color;
+
+        Ball(){
+            radius = (int) random(5,20);
+            x = (int) random(width-radius);
+            y = (int) random(height-radius);
+            speedx = random(0.8f,3.8f);
+            speedy = random(0.8f, 3.8f);
+            color = new int[] {(int)random(255), (int)random(255), (int)random(255)};
+        }
+        public void move(){
+            fill(color[0],color[1],color[2]);
+            ellipse(x, y, radius, radius);
+
+            if (x >= width || x <= 0) {
+                speedx = speedx * -1;
+            }
+            if (y >= width || y <= 0) {
+                speedy = speedy * -1;
+            }
+            x += speedx;
+            y += speedy;
+        }
+    }
+    ArrayList<Ball> balls;
+
+    public void settings() {
+        size(400, 400);
+        balls = new ArrayList<>();
+        for(int i=0;i<100;i++){
+            balls.add(new Ball());
+        }
+    }
+    public void draw() {
+        background(0);
+        for(Ball ball:balls){
+            ball.move();
+        }
+    }
+
+
+
+
+    public static void main (String[]args){
+        String[] processingArgs = {"Balls"};
+        Balls balls = new Balls();
+        PApplet.runSketch(processingArgs, balls);
+    }
+}
